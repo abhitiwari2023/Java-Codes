4dbc455fa722bd7f4d8a39c5da544e7b585ed908
Abhishek Tiwari <abhishek748932@gmail.com>
2024-02-20 22:23:05 +0530

    liner 1D array done

--- src/linear/search/LinerSearch1DEx2.java
+++ src/linear/search/LinerSearch1DEx2.java
@@ -1,25 +1,31 @@
 package linear.search;
 
-public class LinearSearch {
+/**
+ * In the first example we are returning boolean value
+ * In This We are going to return boolean value
+ *
+ * @author Abhishek Tiwari
+ */
+public class LinerSearch1DEx2 {
     public static void main(String[] args) {
         int[] arr = {21, 44, 56, 78, 34, 90, 86, 90}; // homogeneous items only in array
         int target = 44; // integer type target
-        int answer = linearSearch(arr, target); // storing the return int value in the int answer variable
+        boolean answer = linearSearch(arr, target); // storing the return  value in the boolean answer variable
         System.out.println(answer); // print the final answer
     }
 
-    static int linearSearch(int[] arr, int target) {
+    static boolean linearSearch(int[] arr, int target) {
         //check that the array is not empty
         if (arr.length == 0) // if array having zero length then return -1
-            return -1;
+            return false;
 
         // by using for loop we are iterating over each and every element in the array
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) // if the current index having the same object or copy of that then return the index number
-                return i;
+                return true;
         }
 
         // if the given target not found in the Array so return -1 by default case
-        return -1;
+        return false;
     }
 }
